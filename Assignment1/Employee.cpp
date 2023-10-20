9a4b502199a823d3b70163edc3af05a2c9ddad8e
G-Code13 <113222314+G-Code13@users.noreply.github.com>
2023-10-19 20:04:52 -0400

    update

--- Assignment1/Employee.cpp
+++ Assignment1/Employee.cpp
@@ -14,11 +14,11 @@ int main(){
     Address A;
     Employee e;
 
-    cout << "\nEmployee e: " << endl;
+    cout << "Employee e: " << endl;
     e.printEmployee();
 
     Name n1("George", "Victor", "Meghabghab");
-    Address a1("603 S. Germantown Rd", "Chattanooga", "TN", "37411");
+    Address a1("903 S. Germantown Blvd", "Chattanooga", "CA", "12345");
     string ssn1 = "987-65-4321";
     Employee e1(n1, a1, ssn1);
 
